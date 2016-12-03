065464f983707aeb45bf14ec44246c8323f5f976
michael.nikolaev <michael.nikolaev@piano.io>
2016-12-03 14:19:37 +0400

    Initial commit

---
+++ src/main/java/TopWordsLurker.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class TopWordsLurker {
+    private final Charset charset = Charset.forName("UTF-8");
+
+    public static void main(String[] args) {
+        TopWordsLurker wordsLurker = new TopWordsLurker();
+        Path path = Paths.get(/*wordsLurker.readPath()*/"/home/mike/IdeaProjects/TopWordsLurker/src/main/resources/text");
+        wordsLurker.readContents(path);
+    }
+
+    private void readContents(Path path) {
+        try (BufferedReader reader = Files.newBufferedReader(path, charset);
+                Scanner scanner = new Scanner(reader)) {
+            ArrayList<String> wordsList = new ArrayList<>();
+            HashMap<String, Integer> wordsListWithAmount = new HashMap<>();
+
+            while (scanner.hasNext()) {
+                wordsList.add(scanner.next());
+            }
+
+            TopWordsLurker.fillHashMap(wordsList, wordsListWithAmount);
+            System.out.println(wordsListWithAmount);
+
+        } catch (NoSuchFileException x) {
+            System.out.println("File with path \"" + path.toString() + "\" not found!");
+        } catch (IOException x) {
+            System.out.println("Unknown error occured");
+            System.err.println(x.toString());
+        }
+    }
+
+    private String readPath() {
+        Console c = System.console();
+        if (c == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+        return c.readLine("Please, enter the file path(relative or absolute)\n");
+    }
+
+    private static HashMap<String, Integer> fillHashMap(ArrayList<String> arrayFrom, HashMap<String, Integer> mapTo) {
+        Integer amount;
+        for (String word : arrayFrom) {
+            if (mapTo.containsKey(word)) {
+                amount = mapTo.get(word) + 1;
+                mapTo.put(word, amount);
+            }
+            else
+                mapTo.put(word, 1);
+        }
+        return mapTo;
+    }
+}
